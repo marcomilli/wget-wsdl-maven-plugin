177a2b13149bb84ef1551bc593dd887e46f0ed1d
mmilli <mmilli@CABEL-DK1869.gruppocabel.eu>
2017-06-09 16:53:24 +0200

    complete plugin functionality

--- src/main/java/org/github/mm/wget/wsdl/WsdlMojo.java
+++ src/main/java/org/github/mm/wget/wsdl/WsdlMojo.java
@@ -1,23 +1,240 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
  */
 package org.github.mm.wget.wsdl;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.shared.model.fileset.FileSet;
+import org.apache.maven.shared.model.fileset.util.FileSetManager;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
- * @author mmilli
+ * @author Milli Marco
  */
+@Mojo(name = "wget-wsdl", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
 public class WsdlMojo extends AbstractMojo{
 
+    /**
+     * Name of root directory where file is downloaded
+     */
+    @Parameter(property = "destination-directory", required = true)
+    private String destinationDirectory;
+    
+    /**
+     * Name of root directory where file is downloaded
+     */
+    @Parameter(property = "keep", defaultValue = "false")
+    private boolean keep;
+    
+    /**
+     * Create a sub directory for any wsdl file if it set 'true'
+     */
+    @Parameter(property = "create-sub-dir", defaultValue = "false")
+    private boolean createSubDir;
+    
+    /**
+     * Set a prefix name to the import file in wsdl
+     */
+    @Parameter(property = "import-prefix-name", defaultValue = "")
+    private String importPrefixName;
+    
+    /**
+     * Set a postfix name to the import file in wsdl
+     */
+    @Parameter(property = "import-postfix-name", defaultValue = "")
+    private String importPostfixName;
+    
+    @Parameter(property = "wsdl-urls", defaultValue = "tipi")
+    private List<String> wsdlUrls;
+    
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        if(!keep){
+            FileSetManager fsm = new FileSetManager();
+            FileSet fs = new FileSet();
+            fs.setDirectory(destinationDirectory);
+            try {
+                fsm.delete(fs);
+            } catch (IOException ex) {
+                Logger.getLogger(WsdlMojo.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+        
+        for (String urlItem : wsdlUrls) {
+            try {
+                String desString = destinationDirectory;
+                URL url = new URL(urlItem);
+                
+                getLog().info("Url: "+ url.toString());
+                
+                String file = WsdlUtils.parseLocationURL( url );
+                getLog().info("Nome file "+file);
+
+                String ext = WsdlUtils.getExtentionFile(file);
+
+                String nomeSubDir = file.replace(ext, "").replace(".", "");
+                getLog().info("Nome dubDirectory: "+nomeSubDir);
+                
+                if(createSubDir){
+                    desString += File.separator + nomeSubDir;
+                    getLog().info("Creo la directory: "+desString);
+                    WsdlUtils.createDirectory(desString);
+                }
+                
+                String fileName = WsdlUtils.parseLocationURL(url);
+                
+                download(url, desString, fileName);
+                
+            } catch (Exception e) {
+                getLog().info("Error: "+e.getCause()+" - "+e.getMessage());
+            }
+        }
+    }
+    
+    
+    private void download(URL url, String destination, String fileName) throws ParserConfigurationException, IOException, SAXException, TransformerException{
+
+        Document doc = WsdlUtils.parseDocument(url);
+        
+        final NodeList wsdlImport = doc.getElementsByTagName("wsdl:import");
+        
+        for( int i=0; i < wsdlImport.getLength() ; ++i ) {
+
+            Element e = (Element) wsdlImport.item(i);
+
+            String location = e.getAttribute("location");
+
+          getLog().info("wsdl:import location=" + location );
+
+            URI uri = WsdlUtils.resolveURI( url, location );    
+            if( uri != null ) {  
+              
+                final URL locationURL = uri.toURL();
+
+                String newLocation = WsdlUtils.addPrefix(fileName, importPrefixName);
+              newLocation = WsdlUtils.addPostfix(fileName, importPostfixName);
+              e.setAttribute("location", newLocation);
+
+              download( locationURL, destination, newLocation);
+            }
+        }
+        
+        final NodeList xsdImport = doc.getElementsByTagName("xsd:import");
+
+        for( int i=0; i < xsdImport.getLength() ; ++i ) {
+
+          Element e = (Element) xsdImport.item(i);
+
+          String location = e.getAttribute("schemaLocation");
+
+          System.out.printf("xsd:import location=[%s]\n", location );
+
+          URI uri = WsdlUtils.resolveURI( url, location );    
+          if( uri != null ) {  
+
+              final URL locationURL = uri.toURL();
+
+              String newLocation = WsdlUtils.addPrefix(fileName, importPrefixName);
+              newLocation = WsdlUtils.addPostfix(fileName, importPostfixName);
+              e.setAttribute("schemaLocation", newLocation);
+
+              download( locationURL, destination, newLocation);
+          }
+        }
+
+        final NodeList xsInclude = doc.getElementsByTagName("xs:include");
+
+        for( int i=0; i < xsInclude.getLength() ; ++i ) {
+
+          Element e = (Element) xsInclude.item(i);
+
+          String location = e.getAttribute("schemaLocation");
+
+          System.out.printf("xsd:include location=[%s]\n", location );
+
+          URI uri = WsdlUtils.resolveURI( url, location );    
+          if( uri != null ) {  
+
+              final URL locationURL = uri.toURL();
+
+              String newLocation = WsdlUtils.addPrefix(fileName, importPrefixName);
+              newLocation = WsdlUtils.addPostfix(fileName, importPostfixName);
+              e.setAttribute("schemaLocation", newLocation);
+
+              download( locationURL, destination, newLocation);
+          }
+        }
+
+        final NodeList xsImport = doc.getElementsByTagName("xs:import");
+
+        for( int i=0; i < xsImport.getLength() ; ++i ) {
+
+          Element e = (Element) xsImport.item(i);
+
+          String location = e.getAttribute("schemaLocation");
+
+          System.out.printf("xs:import location=[%s]\n", location );
+
+          URI uri = WsdlUtils.resolveURI( url, location );    
+          if( uri != null ) {  
+
+              final URL locationURL = uri.toURL();
+
+              String newLocation = WsdlUtils.addPrefix(fileName, importPrefixName);
+              newLocation = WsdlUtils.addPostfix(fileName, importPostfixName);
+              e.setAttribute("schemaLocation", newLocation);
+
+              download( locationURL, destination, newLocation);
+          }
+        }
+        
+        final NodeList xsRedefine = doc.getElementsByTagName("xs:redefine");
+
+        for( int i=0; i < xsRedefine.getLength() ; ++i ) {
+
+          Element e = (Element) xsRedefine.item(i);
+
+          String location = e.getAttribute("schemaLocation");
+
+          System.out.printf("xs:redefine location=[%s]\n", location );
+
+          URI uri = WsdlUtils.resolveURI( url, location );    
+          if( uri != null ) {  
+
+              final URL locationURL = uri.toURL();
+
+              String newLocation = WsdlUtils.addPrefix(fileName, importPrefixName);
+              newLocation = WsdlUtils.addPostfix(fileName, importPostfixName);
+              e.setAttribute("schemaLocation", newLocation);
+
+              download( locationURL, destination, newLocation);
+          }
+        }
+        
+        fileName = destination + File.separator + fileName;
+        getLog().info("Path + Nome file "+fileName);
+        WsdlUtils.writeXmlFile( doc, fileName );
+
     }
     
 }
---
+++ src/main/java/org/github/mm/wget/wsdl/WsdlUtils.java
@@ -0,0 +1,167 @@
+/*
+ *
+ */
+package org.github.mm.wget.wsdl;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+/**
+ * Utiliy class for manage files, urls and document
+ * 
+ * @author Milli Marco
+ */
+public class WsdlUtils {
+
+    public static String parseLocationURL( URL location ) {
+        if( location==null ) throw new IllegalArgumentException( "location is null!");
+
+        String file = location.getFile();
+
+        int index = file.lastIndexOf('/');
+        if( index!=-1 ) {
+            file = file.substring(++index);
+        }
+
+        file = file.replace('?', '.').replace('=', '.');
+
+        return file;
+    }
+    
+    public static String getExtentionFile(String fileNanme){
+        String ext = fileNanme;
+        int index = fileNanme.lastIndexOf('.');
+        
+        if( index!=-1 ) {
+            ext = fileNanme.substring(++index);
+        }
+        
+        return ext;
+    }
+    
+    public static boolean createDirectory(String path){
+        boolean ret = false;
+        Path realDest = Paths.get(path);
+        try {
+            Files.createDirectory(realDest);
+            ret = true;
+        } catch (IOException ex) {
+            Logger.getLogger(WsdlUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
+    }
+    
+    public static Document parseDocument( URL url ) throws ParserConfigurationException, IOException, SAXException {
+
+        if( url==null ) throw new IllegalArgumentException( "url is null!");
+
+        DocumentBuilder db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+        System.out.printf("download url [%s]\n", url );
+
+        InputStream is = url.openStream();
+
+        String file = url.getFile();
+
+        int index = file.lastIndexOf('/');
+        if( index!=-1 ) {
+            file = file.substring(++index);
+        }
+
+        file = file.replace('?', '.').replace('=', '.');
+
+        Document doc = db.parse( is );
+
+        doc.getDocumentElement().normalize();
+
+        is.close();
+
+        return doc;
+    }
+    
+    public static URI resolveURI( java.net.URL from, String to )  {
+
+        try { 
+            java.net.URI locationURI = new java.net.URI( to );
+
+            if( locationURI.isAbsolute() ) {
+                return locationURI;
+            }
+
+            return from.toURI().resolve(locationURI);
+        
+        } catch( URISyntaxException e ) {
+
+            System.out.printf("error parsing URI [%s]. It will be ignored! \n", to );
+            e.printStackTrace( System.err );
+            
+        }
+        return null;
+    }
+    
+    public static String addPrefix(String filename, String prefix){
+        if(null != filename && 0 < filename.length()){
+            return prefix +"_"+filename;
+        }
+        return "";
+    }
+    
+    public static String addPostfix(String filename, String postfix){
+        if(null != filename &&  0 < filename.length()){
+            String ext = getExtentionFile(filename);
+            int index = filename.lastIndexOf(ext) -1;
+            filename = filename.substring(0, index);
+            return filename + "_" +postfix+"."+ext;
+        }
+        return "";
+    }
+    
+    public static void writeXmlFile(Document doc, String filename) throws TransformerConfigurationException, TransformerException {
+            File file = new File(filename);
+
+            if( file.exists() ) {
+                
+                System.out.printf( "file [%s] already exists. I delete it!\n", file );
+                
+                file.delete();
+                
+//                return;
+            }
+            // Prepare the DOM document for writing
+            Source source = new DOMSource(doc);
+
+            // Prepare the output file
+            Result result = new StreamResult(file);
+
+            // Write the DOM document to the file
+            Transformer xformer = TransformerFactory.newInstance().newTransformer();
+
+            System.out.printf("writing file [%s]\n", file );
+
+            
+            xformer.transform(source, result);
+
+    }
+}
