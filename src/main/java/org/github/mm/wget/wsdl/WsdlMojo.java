35e9ea7b41d40a4155917c58a4d86f6885b1e453
mmilli <mmilli@CABEL-DK1869.gruppocabel.eu>
2017-06-09 15:08:17 +0200

    create mojo class for download wsdl file

---
+++ src/main/java/org/github/mm/wget/wsdl/WsdlMojo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.github.mm.wget.wsdl;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ *
+ * @author mmilli
+ */
+public class WsdlMojo extends AbstractMojo{
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
